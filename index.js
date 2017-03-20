73a26334174a2bdbfc40618def41f15a4ffc6c05
francesco <francesco.viglino@webhub.it>
2017-03-20 12:23:51 +0100

    try run as worker

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const apiKey = process.env.apiKey;
+const port  = process.env.PORT || 80;
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -8,11 +9,11 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content === 'ping') {
-  message.reply('pong');
-} else if (message.content === 'what is my avatar') {
-  // send the user's avatar URL
-  message.reply(message.author.avatarURL);
-}
+    message.reply('pong');
+  } else if (message.content === 'what is my avatar') {
+    // send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
 });
 
 client.login(apiKey);
