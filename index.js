6655a17c0ff04e226916e94e4fb45764573ae6f0
francesco <francesco.viglino@webhub.it>
2017-03-20 12:51:16 +0100

    logs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if(!Config.hasOwnProperty("commandPrefix")){
 let commands = {
   "ping": {
     description: "responds pong, useful for checking if bot is alive",
-    process: function(bot, msg, suffix) {
+    process: function(client, msg, suffix) {
       msg.channel.sendMessage( msg.author+" pong!");
       if(suffix){
         msg.channel.sendMessage( "note that !ping takes no arguments!");
@@ -20,38 +20,29 @@ let commands = {
   "idle": {
     usage: "[status]",
     description: "sets bot status to idle",
-    process: function(bot,msg,suffix){
-      bot.user.setStatus("idle");
-      bot.user.setGame(suffix);
+    process: function(client,msg,suffix){
+      client.user.setStatus("idle");
+      client.user.setGame(suffix);
     }
   },
   "online": {
     usage: "[status]",
     description: "sets bot status to online",
-    process: function(bot,msg,suffix){
-      bot.user.setStatus("online");
-      bot.user.setGame(suffix);
+    process: function(client,msg,suffix){
+      client.user.setStatus("online");
+      client.user.setGame(suffix);
     }
   },
   "say": {
     usage: "<message>",
     description: "bot says message",
-    process: function(bot,msg,suffix){ msg.channel.sendMessage(suffix);}
-  },
-  "announce": {
-    usage: "<message>",
-    description: "bot says message with text to speech",
-    process: function(bot,msg,suffix){ msg.channel.sendMessage(suffix,{tts:true});}
+    process: function(client,msg,suffix){ msg.channel.sendMessage(suffix);}
   },
   "eval": {
     usage: "<command>",
     description: 'Executes arbitrary javascript in the bot process. User must have "eval" permission',
-    process: function(bot,msg,suffix) {
-      if(Permissions.checkPermission(msg.author,"eval")){
-        msg.channel.sendMessage( eval(suffix,bot));
-      } else {
-        msg.channel.sendMessage( msg.author + " doesn't have permission to execute eval!");
-      }
+    process: function(client,msg,suffix) {
+        msg.channel.sendMessage( eval(suffix,client));
     }
   }
 };
@@ -69,6 +60,7 @@ client.login(apiKey);
 function checkMessageForCommand(msg, isEdit) {
   //check if message is a command
   if(msg.author.id != client.user.id && (msg.content.startsWith(Config.commandPrefix))){
+    console.log("treating " + msg.content + " from " + msg.author + " as command");
     let cmdTxt = msg.content.split(" ")[0].substring(Config.commandPrefix.length);
     let suffix = msg.content.substring(cmdTxt.length+Config.commandPrefix.length+1);//add one for the ! and one for the space
     let cmd = commands[cmdTxt];
